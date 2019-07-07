8eeb37a59f024b3fcb1801b752c794e24ed456ff
Gideon Ibemere, Jr <gideon.ibemere.jr@gmail.com>
2019-07-07 02:45:03 -0500

    Remove catch-all route. Unecessary for decoupled app

--- server.js
+++ server.js
@@ -9,19 +9,15 @@ const cors = require('cors')
 require('dotenv').config()
 require('./config/database')
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')))
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Put API routes here, before the "catch all" route
 app.use('/api/scores', scoresApiRouter)
-// The following "catch all" route (note the *)is necessary
-// for a SPA's client-side routing to properly work
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-})
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid coll`ision with React's dev server
